d134dd2a08e0aad3d2ad5d885463562d6b860d9c
User <User@DESKTOP-OTC43P8>
2024-01-21 09:20:59 +0200

    Add project files.

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,39 @@
+﻿using ChatApp.Models.Message;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> 
+            s_messages = new List<KeyValuePair<string, string>>();
+        public IActionResult Show()
+        {
+            if (s_messages.Count == 0)
+            {
+                return View(new ChatViewModel()); //празно съобщение връщало
+            }
+
+            var chatViewModel = new ChatViewModel()
+            {
+                Messages = s_messages
+                .Select(m => new MessageViewModel() {
+                    Sender = m.Key,
+                    MessageText = m.Value
+                })
+                .ToList()
+            };
+            return View(chatViewModel);
+        }
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            var newMessage = chat.CurrentMessage;
+            s_messages.Add(new KeyValuePair<string, string>
+                (newMessage.Sender, newMessage.MessageText));
+            
+
+            return RedirectToAction("Show");
+        }
+    }
+}
